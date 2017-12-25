0acbd1a36004396eada951dc5d2da0551f850a27
MasterUNG <phrombutr@gmail.com>
2017-12-25 14:17:45 +0700

    Noti Step 1

--- app/src/main/java/com/example/administrator/cebcs/ServiceActivity.java
+++ app/src/main/java/com/example/administrator/cebcs/ServiceActivity.java
@@ -1,5 +1,8 @@
 package com.example.administrator.cebcs;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -20,10 +23,15 @@ import com.example.administrator.cebcs.fragment.ServiceFragment;
 import com.example.administrator.cebcs.unity.GetNotiWhereID;
 import com.example.administrator.cebcs.unity.MyConstant;
 import com.example.administrator.cebcs.unity.MyGetAllData;
+import com.example.administrator.cebcs.unity.MyReceiver;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Random;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -31,6 +39,8 @@ public class ServiceActivity extends AppCompatActivity {
     private String[] loginStrings;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private String notificationDateString, fistNotiString,
+            secondNotiString, lastNotiString, idSubjectString, detailString;
 
 
     @Override
@@ -88,11 +98,93 @@ public class ServiceActivity extends AppCompatActivity {
             String currentDateString = dateFormat.format(calendar.getTime());
             Log.d(tag, "currentTime ==> " + currentDateString);
 
+            JSONArray jsonArray = new JSONArray(resultJSON);
+
+            boolean b = true;   //True ==> Find First Date
+
+            for (int i=0; i<jsonArray.length(); i+=1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                int intResult = currentDateString.compareTo(jsonObject.getString("MyDate"));
+                Log.d(tag, "intResult(" + i + ") ==> " + intResult);
+
+                if ((intResult <= 0) && b) {
+
+                    b = false;
+                    notificationDateString = jsonObject.getString("MyDate");
+                    fistNotiString = jsonObject.getString("FirstNoti");
+                    secondNotiString = jsonObject.getString("SecondNoti");
+                    lastNotiString = jsonObject.getString("LastNoti");
+                    idSubjectString = jsonObject.getString("id_Subject");
+                    detailString = jsonObject.getString("Detail");
+
+                }
+
+            }   // for
+
+            Log.d(tag, "notiDate ==> " + notificationDateString);
+
+            Calendar notiCalendar1 = Calendar.getInstance();
+            notiCalendar1.setTime(dateFormat.parse(notificationDateString));
+
+//            Check FirstNoti
+            if (Integer.parseInt(fistNotiString) == 0) {
+
+//                Set Before 3 Day
+                int intNotiDayOfYear = notiCalendar1.get(Calendar.DAY_OF_YEAR);
+                intNotiDayOfYear = intNotiDayOfYear - 3;
+                notiCalendar1.set(Calendar.DAY_OF_YEAR, intNotiDayOfYear);
+                notiCalendar1.set(Calendar.HOUR_OF_DAY, 8);
+
+//                For Test
+
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE) + 1);
+                Log.d(tag, "TestCalendar ==> " + calendar1.getTime());
+
+//                End Test
+
+                myNotification(calendar1, 1);
+
+
+
+
+            } else {
+            }
+
+
+//            calendar.setTime(dateFormat.parse(notificationDateString));
+//            Log.d(tag, "notiCallendar ==> " + calendar.getTime().toString());
+//
+//            int intFirstNotiDate = calendar.get(Calendar.DAY_OF_YEAR);
+//            calendar.set(Calendar.DAY_OF_YEAR, intFirstNotiDate-3);
+//            Log.d(tag, "notiCallendar First ==> " + calendar.getTime().toString());
+
 
 
         } catch (Exception e) {
             Log.d(tag, "e ==> " + e.toString());
         }
+
+    }
+
+    private void myNotification(Calendar notiCalendar1, int indexOfNoti) {
+
+        int i = 0;
+        Random random = new Random();
+        i = random.nextInt(1000);
+
+        Intent intent = new Intent(getBaseContext(), MyReceiver.class);
+
+        intent.putExtra("idSubject", idSubjectString);
+        intent.putExtra("Detail", detailString);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(),
+                i, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, notiCalendar1.getTimeInMillis(), pendingIntent);
+
 
     }
 
